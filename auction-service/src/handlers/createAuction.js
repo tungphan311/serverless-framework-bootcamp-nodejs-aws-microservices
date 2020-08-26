31e5dd25bf9f25d500fa344210fcf971a8eed3b1
Phan Thanh Tùng <tung@Phans-MacBook-Pro.local>
2020-08-26 22:51:57 +0700

    configure yaml to create dynamic table name

--- auction-service/src/handlers/createAuction.js
+++ auction-service/src/handlers/createAuction.js
@@ -1,12 +1,25 @@
+import { v4 as uuid } from "uuid";
+import AWS from "aws-sdk";
+
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
+
 async function createAuction(event, context) {
   const { title } = JSON.parse(event.body);
   const now = new Date();
 
   const auction = {
+    id: uuid(),
     title,
     status: "OPEN",
     createAt: now.toISOString(),
   };
+
+  await dynamoDB
+    .put({
+      TableName: process.env.AUCTIONS_TABLE_NAME,
+      Item: auction,
+    })
+    .promise();
 
   return {
     statusCode: 201,
